d362b582dd660d17e3e1d002cc4e2ab972d9590d
Elinor Chayo <elliechayo@gmail.com>
2023-03-29 22:30:22 -0400

    added the array of questions fo r user input, and a write README file function

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,72 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+//  packages included for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
+//  an array of questions for user input
+const questions = [
+     {
+          type: "input",
+          name: "title",
+          message: "Enter title of your project: ",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Enter descrition: ",
+        },
+        {
+          type: "input",
+          name: "installation",
+          message: "Enter installation instruction: ",
+        },
+        {
+          type: "input",
+          name: "usage",
+          message: "Enter usage information: ",
+        },
+        {
+          type: "input",
+          name: "contribution",
+          message: "Enter contribution guidelines: ",
+        },
+        {
+          type: "input",
+          name: "test",
+          message: "Enter test instructions: ",
+        },
+        {
+          type: "input",
+          name: "reponame",
+          message: "Enter repo name: ",
+        },
+        {
+          type: "input",
+          name: "username",
+          message: "Enter Github username: ",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter your email address: ",
+        },
+        {
+          type: "list",
+          name: "license",
+          message: "Choose a license",
+          choices: [
+            { name: "No License", value: "" },
+            "Academic Free License v3.0",
+            "Apache license 2.0",
+            "BSD 3-clause Clear license",
+            "Creative Commons license family",
+            "GN General Public License v3.0",
+            "ISC",
+            "MIT",
+            "Open Software License 3.0",
+          ],
+        },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
